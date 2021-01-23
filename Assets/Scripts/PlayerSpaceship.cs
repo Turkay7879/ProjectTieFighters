8a474632650ba70ac027bd40cadad4103755ecb2
burakcolak00 <burakcolak000@gmail.com>
2021-01-24 00:42:29 +0300

    hareket
    velocity kullanıldı

--- Assets/Scripts/PlayerSpaceship.cs
+++ Assets/Scripts/PlayerSpaceship.cs
@@ -6,10 +6,13 @@ public class PlayerSpaceship : MonoBehaviour
 {
     public float Speed = 10.0f;
     Vector2 position;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position;
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,7 +23,7 @@ public class PlayerSpaceship : MonoBehaviour
 
     void Movement()
     {
-        
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, 0);
     }
 
     void Fire()
