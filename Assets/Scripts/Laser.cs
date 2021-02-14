280ce1eacef5bd948f517ca45192a90381114649
Costanza15 <tahsinemirhan@hotmail.com>
2021-02-15 00:43:59 +0300

    Create Laser.cs

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    GameManagement Management;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Management = GameObject.Find("GameManagement").GetComponent<GameManagement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Enemy"))
+        {
+            Destroy(GameObject.Find(collision.name));
+            Destroy(GameObject.Find("Laser1(Clone)"));
+            int tempCount = Management.EnemyCount;
+            tempCount--;
+            Management.EnemyCount = tempCount;
+        }
+    }
+}
